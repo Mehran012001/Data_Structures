75f35dec65b7eaa0da0ca5276af462bae76cf591
Mehran Zahoor <107433204+Mehran012001@users.noreply.github.com>
2023-10-18 23:09:57 +0530

    BST.c

---
+++ bst.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return newNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+int search(struct Node* root, int key) {
+    if (root == NULL || root->data == key) {
+        return root != NULL;
+    }
+
+    if (key < root->data) {
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Inorder traversal: ");
+    inorder(root);
+
+    int key = 40;
+    if (search(root, key)) {
+        printf("\n%d found in the BST.\n", key);
+    } else {
+        printf("\n%d not found in the BST.\n", key);
+    }
+
+    return 0;
+}
